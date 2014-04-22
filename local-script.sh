84b84bc45f9408798cbf7dd62c84edb76f722a88
simon davies <simondavies@live.co.uk>
2014-04-22 20:49:18 +0100

    Created an override file for the vaprobash set up so that it sets the xdebug scream value to 0, to help with a laravel install etc

---
+++ local-script.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+echo ">>> Updating xdebug so that scream is set to 0"
+
+# xdebug Config
+cat > $(find /etc/php5 -name xdebug.ini) << EOF
+zend_extension=$(find /usr/lib/php5 -name xdebug.so)
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9000
+xdebug.scream=0
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+
+; var_dump display
+xdebug.var_display_max_depth = 5
+xdebug.var_display_max_children = 256
+xdebug.var_display_max_data = 1024
+EOF
